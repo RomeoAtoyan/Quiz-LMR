ea1573210c8d94a848d30d79344be36a9e03be63
RomeoAtoyan <atoyanr@gmail.com>
2026-05-15 13:57:31 +0200

    feat: add quiz game view

---
+++ src/components/quiz/QuizView.tsx
@@ -0,0 +1,15 @@
+import Badge from "../ui/Badge";
+
+const QuizView = () => {
+  return (
+    <div className="flex flex-col gap-4 p-8 bg-primary min-h-screen">
+      <div className="flex gap-4">
+        <Badge variant="white">
+          <span className="mr-2">⏱️</span> 0:20
+        </Badge>
+      </div>
+    </div>
+  );
+};
+
+export default QuizView;
